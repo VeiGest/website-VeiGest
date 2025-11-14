5b0846801befcc73b2b8da834f4529f66ac38acd
leonardopereira <2240103@my.ipleiria.pt>
2025-11-14 18:48:38 +0000

    implementacao do esqueceu a senha

--- veigest/frontend/views/site/resetPassword.php
+++ veigest/frontend/views/site/resetPassword.php
@@ -1,31 +1,26 @@
 <?php
-
-/** @var yii\web\View $this */
-/** @var yii\bootstrap5\ActiveForm $form */
-/** @var \frontend\models\ResetPasswordForm $model */
-
-use yii\bootstrap5\Html;
 use yii\bootstrap5\ActiveForm;
+use yii\bootstrap5\Html;
 
-$this->title = 'Reset password';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Nova Palavra-passe';
 ?>
-<div class="site-reset-password">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please choose your new password:</p>
+<section class="bg-gray-50 dark:bg-gray-900 min-h-screen flex items-center justify-center">
+  <div class="w-full max-w-md bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
+      <div class="p-6 space-y-6">
+          <h1 class="text-2xl font-bold text-center text-gray-900 dark:text-white"><?= Html::encode($this->title) ?></h1>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
+          <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
 
-                <?= $form->field($model, 'password')->passwordInput(['autofocus' => true]) ?>
+              <?= $form->field($model, 'password')->passwordInput([
+                  'class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white',
+              ])->label('Nova palavra-passe', ['class' => 'text-sm font-medium text-gray-900 dark:text-white']) ?>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
-                </div>
+              <div class="form-group text-center">
+                  <?= Html::submitButton('Redefinir palavra-passe', ['class' => 'w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5']) ?>
+              </div>
 
-            <?php ActiveForm::end(); ?>
-        </div>
-    </div>
-</div>
+          <?php ActiveForm::end(); ?>
+      </div>
+  </div>
+</section>
